4e4345a7b92d3c297cd21d8bd5df5ef6ba2decd4
badhaloninja <badhaloninja@badhalo.ninja>
2022-03-26 02:41:39 -0500

    Quick work around for UserJoin and UserLeave events only triggering if host

--- UserJoinLeaveNotifications/UserJoinLeaveNotifications.cs
+++ UserJoinLeaveNotifications/UserJoinLeaveNotifications.cs
@@ -4,7 +4,7 @@ using NeosModLoader;
 using System;
 using BaseX;
 using System.Reflection;
-
+using FrooxEngine.LogiX.WorldModel;
 namespace UserJoinLeaveNotifications
 {
     public class UserJoinLeaveNotifications : NeosMod
@@ -25,10 +25,11 @@ namespace UserJoinLeaveNotifications
             public static void Postfix(NotificationPanel __instance)
             {
                 new NewNotificationHandler().Setup(__instance);
+
             }
 
-            [HarmonyReversePatch(HarmonyReversePatchType.Snapshot)] // Snapshot incase you are using the DesktopNotifications mod
             // HarmonyReversePatch has to be under a harmony patch class???
+            [HarmonyReversePatch(HarmonyReversePatchType.Snapshot)] // Snapshot incase you are using the DesktopNotifications mod
             [HarmonyPatch(typeof(NotificationPanel), "AddNotification", new Type[] { typeof(string), typeof(string), typeof(Uri), typeof(color), typeof(string), typeof(Uri), typeof(IAssetProvider<AudioClip>) })]
             public static void AddNotification(NotificationPanel instance, string userId, string message, Uri thumbnail, color backgroundColor, string mainMessage, Uri overrideProfile, IAssetProvider<AudioClip> clip)
             {
@@ -53,24 +54,37 @@ namespace UserJoinLeaveNotifications
             private void cleanUp(IChangeable changeable)
             {
                 oldFocusedWorld.WorldManager.WorldFocused -= OnWorldFocused;
-                oldFocusedWorld.UserJoined -= OnUserJoined;
-                oldFocusedWorld.UserLeft -= OnUserLeft;
+
+                /* Join leave events only work as host 
+                 * 
+                 * Interfacing with the UserBag here so I don't have to manually check for changes in some loop
+                 * and to avoid patching core functions
+                 * 
+                 */
+                
+                getUserbag(oldFocusedWorld).OnElementAdded -= OnUserJoined;
+                getUserbag(oldFocusedWorld).OnElementRemoved -= OnUserLeft;
+                /*oldFocusedWorld.UserJoined -= OnUserJoined;
+                oldFocusedWorld.UserLeft -= OnUserLeft;*/
             }
             private void OnWorldFocused(World newFocusedWorld)
             {
-                if(oldFocusedWorld != null)
+                if (oldFocusedWorld != null)
                 {
-                    oldFocusedWorld.UserJoined -= OnUserJoined;
-                    oldFocusedWorld.UserLeft -= OnUserLeft;
+                    getUserbag(oldFocusedWorld).OnElementAdded -= OnUserJoined;
+                    getUserbag(oldFocusedWorld).OnElementRemoved -= OnUserLeft;
+                    /*oldFocusedWorld.UserJoined -= OnUserJoined;
+                    oldFocusedWorld.UserLeft -= OnUserLeft;*/
                 }
-                
 
-                newFocusedWorld.UserJoined += OnUserJoined;
-                newFocusedWorld.UserLeft += OnUserLeft;
+                getUserbag(newFocusedWorld).OnElementAdded += OnUserJoined;
+                getUserbag(newFocusedWorld).OnElementRemoved += OnUserLeft;
+                /*newFocusedWorld.UserJoined += OnUserJoined;
+                newFocusedWorld.UserLeft += OnUserLeft;*/
 
                 oldFocusedWorld = newFocusedWorld;
             }
-            private void OnUserJoined(User user)
+            private void OnUserJoined(SyncBagBase<RefID, User> bag, RefID key, User user, bool isNew)
             {
                 if (user.IsLocalUser) return;
                 notificationPanel.RunSynchronously(() =>
@@ -78,7 +92,7 @@ namespace UserJoinLeaveNotifications
                     notifPanelPatch.AddNotification(notificationPanel, user.UserID, String.Format("{0} joined", user.UserName), null, MathX.Lerp(color.Blue, color.White, 0.5f), "N/A", null, null);
                 });
             }
-            private void OnUserLeft(User user)
+            private void OnUserLeft(SyncBagBase<RefID, User> bag, RefID key, User user)
             {
                 if (user.IsLocalUser) return;
                 notificationPanel.RunSynchronously(() =>
@@ -86,6 +100,11 @@ namespace UserJoinLeaveNotifications
                     notifPanelPatch.AddNotification(notificationPanel, user.UserID, String.Format("{0} left", user.UserName), null, MathX.Lerp(color.Red, color.White, 0.5f), "N/A", null, null);
                 });
             }
+            private static UserBag getUserbag(World world)
+            { // Return the user bag field
+                return Traverse.Create(world).Field("_users").GetValue<UserBag>();
+            }
         }
+        
     }
 }
