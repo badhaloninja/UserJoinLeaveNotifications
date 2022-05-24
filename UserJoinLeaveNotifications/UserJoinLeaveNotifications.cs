bb4aa3db14e98a0cc65414ceeb140279d7baa368
badhaloninja <badhaloninja@badhalo.ninja>
2022-05-24 13:06:43 -0500

    Solved issue of ReversePatch not working as intended with harmony versions past v2.1.0.0 By removing patches :)

--- UserJoinLeaveNotifications/UserJoinLeaveNotifications.cs
+++ UserJoinLeaveNotifications/UserJoinLeaveNotifications.cs
@@ -4,109 +4,98 @@ using NeosModLoader;
 using System;
 using BaseX;
 using System.Reflection;
-using FrooxEngine.LogiX.WorldModel;
+
+using System.Threading.Tasks;
+
 namespace UserJoinLeaveNotifications
 {
     public class UserJoinLeaveNotifications : NeosMod
     {
         public override string Name => "UserJoinLeaveNotifications";
         public override string Author => "badhaloninja";
-        public override string Version => "1.1.1";
+        public override string Version => "1.2.0";
         public override string Link => "https://github.com/badhaloninja/UserJoinLeaveNotifications";
         public override void OnEngineInit()
         {
-            Harmony harmony = new Harmony("me.badhaloninja.UserJoinLeaveNotifications");
-            harmony.PatchAll();
-        }
-
-        [HarmonyPatch(typeof(NotificationPanel), "OnAttach")]
-        class notifPanelPatch
-        {
-            public static void Postfix(NotificationPanel __instance)
-            {
-                new NewNotificationHandler().Setup(__instance);
-            }
-
-            // HarmonyReversePatch has to be under a harmony patch class???
-            [HarmonyReversePatch(HarmonyReversePatchType.Snapshot)] // Snapshot incase you are using the DesktopNotifications mod
-            [HarmonyPatch(typeof(NotificationPanel), "AddNotification", new Type[] { typeof(string), typeof(string), typeof(Uri), typeof(color), typeof(string), typeof(Uri), typeof(IAssetProvider<AudioClip>) })]
-            public static void AddNotification(NotificationPanel instance, string userId, string message, Uri thumbnail, color backgroundColor, string mainMessage, Uri overrideProfile, IAssetProvider<AudioClip> clip)
-            {
-                throw new NotImplementedException("It's a stub");
-            }
-        }
-
-        class NewNotificationHandler
-        { // To handle the small chance that the functions would need to be removed from the events incase the component gets deleted (very unlikely under normal circumstances)
-            private NotificationPanel notificationPanel;
-            private World oldFocusedWorld;
-
-            public void Setup(NotificationPanel instance)
-            {
-                notificationPanel = instance;
-
-                instance.Engine.WorldManager.WorldFocused += OnWorldFocused;
-
-                instance.Destroyed += cleanUp;
-            }
-
-            private void cleanUp(IChangeable changeable)
-            {
-                oldFocusedWorld.WorldManager.WorldFocused -= OnWorldFocused;
-
-                /* Join leave events only work as host 
-                 * 
-                 * Interfacing with the UserBag here so I don't have to manually check for changes in some loop
-                 * and to avoid patching core functions
-                 * 
-                 */
-                
-                GetUserbag(oldFocusedWorld).OnElementAdded -= OnUserJoined;
-                GetUserbag(oldFocusedWorld).OnElementRemoved -= OnUserLeft;
-            }
-            private void OnWorldFocused(World newFocusedWorld)
-            {
-                if (oldFocusedWorld != null)
-                {
-                    GetUserbag(oldFocusedWorld).OnElementAdded -= OnUserJoined;
-                    GetUserbag(oldFocusedWorld).OnElementRemoved -= OnUserLeft;
-                }
-                GetUserbag(newFocusedWorld).OnElementAdded += OnUserJoined;
-                GetUserbag(newFocusedWorld).OnElementRemoved += OnUserLeft;
-
-                oldFocusedWorld = newFocusedWorld;
-            }
-            private void OnUserJoined(SyncBagBase<RefID, User> bag, RefID key, User user, bool isNew)
-            {
-                if (user.IsLocalUser) return;
-                notificationPanel.RunInUpdates(3, async () =>
-                { // Running immediately results in the getuser to return a BadRequest
-                    var cloudUserProfile = (await Engine.Current.Cloud.GetUser(user.UserID))?.Entity?.Profile;
-                    // Handle fetching profile, AddNotification only gets profile data for friends
-                    var thumbnail = CloudX.Shared.CloudXInterface.TryFromString(cloudUserProfile?.IconUrl) ?? NeosAssets.Graphics.Thumbnails.AnonymousHeadset;
-                    notificationPanel.RunSynchronously(() =>
-                    { // Events are nonlocking threads
-                        notifPanelPatch.AddNotification(notificationPanel, null, string.Format("{0} joined", user.UserName), null, MathX.Lerp(color.Blue, color.White, 0.5f), "User Joined", thumbnail, null);
-                    });
-                });
-                
-            }
-            private async void OnUserLeft(SyncBagBase<RefID, User> bag, RefID key, User user)
-            {
-                if (user.IsLocalUser) return;
-                var cloudUserProfile = (await Engine.Current.Cloud.GetUser(user.UserID))?.Entity?.Profile;
-                // Handle fetching profile, AddNotification only gets profile data for friends
-                var thumbnail = CloudX.Shared.CloudXInterface.TryFromString(cloudUserProfile?.IconUrl) ?? NeosAssets.Graphics.Thumbnails.AnonymousHeadset;
-                notificationPanel.RunSynchronously(() =>
-                { // Events are nonlocking threads
-                    notifPanelPatch.AddNotification(notificationPanel, null, string.Format("{0} left", user.UserName), null, MathX.Lerp(color.Red, color.White, 0.5f), "User Left", thumbnail, null);
-                });
-            }
-            private static UserBag GetUserbag(World world)
-            { // Return the user bag field
-                return Traverse.Create(world).Field("_users").GetValue<UserBag>();
-            }
+            Engine.Current.RunPostInit(Setup);
         }
         
+
+        private static MethodInfo addNotification;
+
+        private static UserBag currentUserBag;
+        
+        
+        public static void Setup()
+        {
+            // Hook into the world focused event
+            Engine.Current.WorldManager.WorldFocused += OnWorldFocused;
+
+            // Store the method info for later use
+            addNotification = AccessTools.Method(typeof(NotificationPanel), "AddNotification", new Type[] { typeof(string), typeof(string), typeof(Uri), typeof(color), typeof(string), typeof(Uri), typeof(IAssetProvider<AudioClip>) });
+        }
+        private static void OnWorldFocused(World focusedWorld)
+        {
+            // User join leave events only trigger as host
+            if (currentUserBag != null)
+            { // Remove the event handler from the old world
+                currentUserBag.OnElementAdded -= OnUserJoined;
+                currentUserBag.OnElementRemoved -= OnUserLeft;
+            }
+            
+            
+            // Store the user bag of the new world
+            currentUserBag = GetUserbag(focusedWorld);
+            // Add the event handler to the new world
+            currentUserBag.OnElementAdded += OnUserJoined;
+            currentUserBag.OnElementRemoved += OnUserLeft;
+        }
+        private static void OnUserJoined(SyncBagBase<RefID, User> bag, RefID key, User user, bool isNew)
+        {
+            if (user.IsLocalUser) return;
+            if (NotificationPanel.Current == null) return;
+            
+            NotificationPanel.Current.RunInUpdates(3, async () =>
+            { // Running immediately results in the getuser to return a BadRequest
+                Uri thumbnail = await GetUserThumbnail(user.UserID);
+                
+                AddNotification(string.Format("{0} joined", user.UserName),MathX.Lerp(color.Blue, color.White, 0.5f), "User Joined", thumbnail);
+            });
+        }
+
+        private static async void OnUserLeft(SyncBagBase<RefID, User> bag, RefID key, User user)
+        {
+            if (user.IsLocalUser) return;
+            if (NotificationPanel.Current == null) return;
+            
+            Uri thumbnail = await GetUserThumbnail(user.UserID);
+            AddNotification(string.Format("{0} left", user.UserName), MathX.Lerp(color.Red, color.White, 0.5f), "User Left", thumbnail);
+        }
+
+        
+        
+        // Async method to fetch thumbnail from user id
+        private static async Task<Uri> GetUserThumbnail(string userId)
+        {
+            var cloudUserProfile = (await Engine.Current.Cloud.GetUser(userId))?.Entity?.Profile;
+            // Handle fetching profile, AddNotification only gets profile data for friends
+            var thumbnail = CloudX.Shared.CloudXInterface.TryFromString(cloudUserProfile?.IconUrl) ?? NeosAssets.Graphics.Thumbnails.AnonymousHeadset;
+            return thumbnail;
+        }
+        
+        private static UserBag GetUserbag(World world)
+        { // Return the user bag field
+            return Traverse.Create(world).Field("_users").GetValue<UserBag>();
+        }
+        
+        private static void AddNotification(string message, color backgroundColor, string mainMessage, Uri overrideProfile)
+        {
+            // Not using Show Notification because it does not expose main message
+            if (NotificationPanel.Current == null) return;
+            NotificationPanel.Current.RunSynchronously(() =>
+            { // ;-;
+                addNotification.Invoke(NotificationPanel.Current, new object[] { null, message, null, backgroundColor, mainMessage, overrideProfile, null });
+            });
+        }
     }
 }
